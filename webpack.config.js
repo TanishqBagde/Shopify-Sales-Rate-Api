522f00254e8f3a9ca7da494f76a38a1be746298b
TanishqBagde <tanishqbagde1618@gmail.com>
2024-08-26 14:43:32 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './app.js', // Adjust to your entry point
+  mode: 'production',
+  entry: './index.js', // Adjust to your entry point
   target: 'node', // Specify that this is for a Node.js application
   externals: [nodeExternals()], // Exclude node_modules from the bundle
   output: {
